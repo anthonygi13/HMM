f30e63b873b01f2fabe6697f8f648b402986df1d
Hii-Chan <hichma.k@hotmail.fr>
2018-05-07 10:37:22 +0200

    fonction verif_entree

--- code/script.py
+++ code/script.py
@@ -6,6 +6,15 @@
 
 from classe import *
 
+
+def verif_entree(entree, texte):
+    while entree - int(entree) != 0 or entree <= 0:
+        print ()
+        print ("Vous devez entrer un entier strictement positif")
+        print ()
+        n = float(input(texte))
+    return entree
+
 listeBW = []
 input("Bonjour. Vous allez découvrir le code réalisé par la Communauté de l\'info pour le projet HMM. Pour continuer appuyez sur entrée.")
 print()
@@ -19,13 +28,10 @@ print()
 var = 'o'
 while var == 'o':
     n = float(input("Veuillez entrer une longueur de mot à générer aléatoire grâce au HMM chargé : "))
-    while n - int(n) != 0 or n <= 0:
-        print ()
-        print ("Vous devez entrer un entier positif")
-        print ()
-        n = float(input("Veuillez entrer une longueur de mot à générer aléatoire grâce au HMM chargé : "))
+    n = verif_entree(n, "Veuillez entrer une longueur de mot à générer aléatoire grâce au HMM chargé : ")
     n = int(n)
     print()
+
     w = h.generate_random(n)
     print("Le mot de", n, "lettre qui a été généré aléatoirement est", w)
     listeBW += [w]
@@ -37,20 +43,20 @@ while var == 'o':
     print("Si l'on continuait à générer des lettres pour ce mot, la prochaine serait le plus probalement", prochaine)
     print ()
     var = input("Voulez-vous générer une autre séquence afin d'utiliser Baum Welch ? o/n")
+
     while var != 'o' and var!= 'n':
         print ("Choisissez o ou n")
         print ()
         var = input("Voulez-vous générer une autre séquence afin d'utiliser Baum Welch ? o/n")
     print ()
+
 nb = float(input("Combien de fois voulez-vous effectuer Baum Welch ?"))
-while nb - int(nb) != 0 or nb < 0:
-    print ()
-    print ("Vous devez entrer un entier positif")
-    print()
-    nb = input ("Combien de fois voulez-vous effectuer Baum Welch ?")
+nb = verif_entree(nb,"Combien de fois voulez-vous effectuer Baum Welch ?" )
 nb = int(nb)
+
 for i in range (nb):
     h.bw1(listeBW)
+
 print()
 print ("Le nouveau HMM dont la vraisemblance a été augmentée", nb, "fois est le suivant :")
 print ()
