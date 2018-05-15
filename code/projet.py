f824b6febeeda4fad37218b35c18801481f8cc6e
Hii-Chan <hichma.k@hotmail.fr>
2018-05-15 14:00:37 +0200

    projet.py

--- code/projet.py
+++ code/projet.py
@@ -1,5 +1,6 @@
 
 from classe import *
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -8,17 +9,34 @@ def text_to_list(adr):
     texte = data.read()
     L = texte.split('\n')
     data.close()
-    return L
+    L2 = []
+    for w in L:
+        w2 = ()
+        for i in range (len(w)):
+            w2 += (HMM.lettre_to_num(w[i]),)
+        L2 += [w2]
+    return L2
 
+def xval(nbFolds, S, nbL, nbSMin, nbSMax, nbIter, nbInit):
+    n = len(S)
+    l = np.random.permutation(n)
+    lvOpt = -float("inf")
+    for nbS in range(nbSMin, nbSMax):
+        lv = 0
+        for i in range(1,nbFolds+1):
+            f1 = int((i-1)*n/nbFolds)
+            f2 = int(i*n/nbFolds)
+            learn = [S[l[j]] for j in range(f1)]
+            learn += [S[l[j]] for j in range(f2,n)]
+            test = [S[l[j]] for j in range(f1,f2)]
+            h = HMM.bw3(nbL,nbS,learn,nbIter,nbInit)
+        lv += h.logV(test)
+        if lv > lvOpt:
+            lvOpt = lv
+            nbSOpt = nbS
+    return lvOpt,nbSOpt
 
-S = text_to_list('allemand2000')[:-1]
-S2 = []
-for w in S :
-    w2 = ()
-    for i in range (len(w)):
-        w2 += (HMM.lettre_to_num(w[i]),)
-    S2 += [w2]
-
+'''''''''''''''
 y = []
 x = []
 for n in range (2,100000000):
@@ -33,4 +51,4 @@ for n in range (2,100000000):
 plt.plot(x, y)
 plt.show()
 
-
+'''''''''''''''''
